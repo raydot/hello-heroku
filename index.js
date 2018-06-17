786d624d8495e0e99d97a8abe5738133c01cc7d3
Dave Kanter <moatgoat@hotmail.com>
2018-06-17 11:53:37 -0700

    Added some use cast testing and initial db creation.

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const http = require('http');
 
-//const hostname = '127.0.0.1';
-//const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-//Run on any port supplied, otherwise fallback to 5000
+// Run on any port supplied, otherwise fallback to 5000
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Dave rules!\n');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Dave rules!\n');
 });
 
 // server.listen(port, hostname, () => {
-// 	console.log(`Server running at http://${hostname}:${port}/`);
+//   console.log(`Server running at http://${hostname}:${port}/`);
 // });
 
 // rewrite server to use PORT
 server.listen(PORT, () => {
-	console.log(`Server running on ${PORT}/`);
-})
+  // disable "no console" warning
+  // eslint-disable-next-line
+  console.log(`Server running on ${PORT}/`);
+});
